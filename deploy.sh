2cf0bd59d04934e2f22eec31a9f18dc28283f2f9
zhengxixuan <zhengxixuan1020@gmail.com>
2021-01-28 22:18:12 +0800

    Generate site

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ set -x
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo  # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t cactus  # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
